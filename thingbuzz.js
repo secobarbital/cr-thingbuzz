2faf476959d870d54e92b3ab0c21c8c3ec5d8250
Seggy Umboh <seggy.umboh@nextag.com>
2012-12-11 16:17:18 -0800

    Render the comments when clicking the post

--- thingbuzz.js
+++ thingbuzz.js
@@ -1,21 +1,32 @@
 var baseUrl = 'http://www.thingbuzz.com';
 
-$('.feed').on('click', 'a', function(e) {
-  chrome.tabs.create({
-    url: $(this).attr('href')
-  });
+function renderFeed(data) {
+  for (var i=0; i < data.feed.length; i++) {
+    var postView = $($('#post-template').html());
+    postView.data('post', data.feed[i]);
+    postView.find('.url').attr('href', baseUrl + '/posts/' + data.feed[i]._id);
+    postView.find('.text').text(data.feed[i].comments[0].comment);
+    $('#feed').append(postView);
+  }
+}
+
+$('#feed').on('click', '.post a.url', function() {
+  var i, comment, commentView, post;
+
+  $('#feed').hide();
+  $('#post').html('');
+  post = $(this).parents('li').data('post');
+  for (i=0; i < post.comments.length; i++) {
+    comment = post.comments[i];
+    commentView = $($('#comment-template').html());
+    commentView.find('.user').text(comment.user.displayName);
+    commentView.find('.text').text(comment.comment);
+    $('#post').append(commentView);
+  }
+  $('#post').show();
 });
 
 $(function() {
-  var renderFeed = function(data) {
-    for (var i=0; i < data.feed.length; i++) {
-      var postView = $($('#post-template').html());
-      postView.find('.url').attr('href', baseUrl + '/posts/' + data.feed[i]._id);
-      postView.find('.text').text(data.feed[i].comments[0].comment);
-      $('#feed').append(postView);
-    }
-  }
-
   chrome.tabs.getSelected(null, function(tab) {
     url = baseUrl + '/products/' + encodeURIComponent(tab.url) + '/feed';
     $.ajax({
