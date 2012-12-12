328bd32ddac3a696d42d416aa874949656677c97
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2012-12-11 18:44:28 -0800

    Display current page question.

--- thingbuzz.js
+++ thingbuzz.js
@@ -2,23 +2,23 @@ var baseUrl = 'http://www.thingbuzz.com';
 
 function renderFeed(data) {
   var i, commentView, commentsView, post, postView;
-
   for (i=0; i < data.feed.length; i++) {
     post = data.feed[i];
+    question = post.comments[0].comment
     postView = $($('#post-template').html());
     postView.attr('id', post._id);
     postView.find('.url').attr('href', '#' + postView.attr('id'));
-    postView.find('.text').text(post.comments[0].comment);
+    postView.find('.text').text(question.replace(/@\[(.+?):(.+?)\]/g, "@$2"));
     commentsView = postView.find('.comments');
     commentsView.attr('id', post._id + '/comments').hide();
-    for (i=1; i < post.comments.length; i++) {
-      comment = post.comments[i];
+    for (j=1; j < post.comments.length; j++) {
+      comment = post.comments[j];
       commentView = $($('#comment-template').html());
       commentView.find('.user').text(comment.user.displayName);
       commentView.find('.text').text(comment.comment);
       commentsView.append(commentView);
     }
-    $('#jqt').append(postView);
+    $('#jqt ul').append(postView);
   }
 }
 
