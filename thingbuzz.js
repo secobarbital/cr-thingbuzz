4ad0a9af7e3b37156165b9d3913e00e3ef42b973
Seggy Umboh <seggy.umboh@nextag.com>
2012-12-13 15:58:17 -0800

    Encode @mentions once

--- thingbuzz.js
+++ thingbuzz.js
@@ -5,18 +5,18 @@ function renderFeed(data) {
 
   for (i=0; i < data.feed.length; i++) {
     post = data.feed[i];
-    question = post.comments[0].comment
+    question = post.comments[0].comment.replace(/@\[(.+?):(.+?)\]/g, "@$2")
     postView = $($('#post-template').html());
     postView.attr('id', post._id);
     postView.find('.url').attr('href', '#' + postView.attr('id') + '-comments');
-    postView.find('.text').text(question.replace(/@\[(.+?):(.+?)\]/g, "@$2"));
+    postView.find('.text').text(question);
     postView.find('.ui-li-count').text(post.comments.length-1);
     $('[data-role="content"] ul').append(postView);
 
     commentsView = $($('#conversation').html())
     commentsView.attr('id', post._id + '-comments');
     commentsView.attr('data-url', post._id + '-comments');
-    commentsView.find('div[data-role="header"] h1').text(question.replace(/@\[(.+?):(.+?)\]/g, "@$2"));
+    commentsView.find('div[data-role="header"] h1').text(question);
     for (j=0; j < post.comments.length; j++) {
       comment = post.comments[j];
       commentView = $($('#comment-template').html());
