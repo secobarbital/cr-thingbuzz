71885a3d912e4d927d737afe0e304086c0c81b79
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2012-12-20 14:41:52 -0800

    Notification for new post

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+var baseUrl = localStorage.baseUrl || 'http://www.thingbuzz.com',
+    socket = io.connect(baseUrl);
+
+socket.emit('room:join', null);
+socket.on('feed:create', function(post) {
+  var notification;
+  notification = webkitNotifications.createNotification(
+    'http://graph.facebook.com/' + post.subject.authHash.id + '/picture',
+    'You\'ve got a new question!',
+    post.subject.authHash.displayName + ' : "' +  post.comments[0].comment + '"'
+  );
+  notification.onclick = function() {
+    chrome.tabs.create({
+      url: post.object ? post.object.links[0] : baseUrl + '/posts/' + post._id
+    }); 
+    this.cancel();
+  }
+  notification.show();
+});
+
 function onUpdated(tabId, changeInfo, tab) {
   if (changeInfo.url && ~changeInfo.url.indexOf('/glade')) {
     chrome.tabs.onUpdated.removeListener(onUpdated);
